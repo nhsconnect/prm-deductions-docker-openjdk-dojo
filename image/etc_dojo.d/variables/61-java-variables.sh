d802c6970c76fe02798b24e07a2084f5d434bcda
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-01-09 15:27:41 +0000

    PRMT-4342 (#1)
    * [PRMT-4342] Updated README.md to reflect new changes.
    
    * [PRMT-4342] Updated Dockerfile to update Java, Gradle and Maven versions.
    
    * [PRMT-4342] Updated 21-java-variables.sh to update Java versions.
    
    * [PRMT-4342] Updated CHANGELOG.md to reflect new changes.

--- image/etc_dojo.d/variables/61-java-variables.sh
+++ image/etc_dojo.d/variables/61-java-variables.sh
@@ -2,6 +2,6 @@
 
 # those are set by a base image, but when using dojo, user may have overwritten
 # them, so set them again:
-export JAVA_HOME=/usr/local/openjdk-17
-export JAVA_VERSION=17.0.3
+export JAVA_HOME=/usr/local/openjdk-21
+export JAVA_VERSION=21-ea+26
 export PATH=$JAVA_HOME/bin:$PATH
